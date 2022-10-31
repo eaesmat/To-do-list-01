9607fd31788ad1495f9300ffd5143fed01e54dfe
Nonhlanhla <nonhlanhlamndebele9@gmail.com>
2022-10-31 10:53:13 +0200

    Revert "Add copy of the project"

--- src/modules/mainContainerClass.js
+++
@@ -1,8 +0,0 @@
-// Main container class - Array of Objects
-class MyToDoList {
-  constructor() {
-    this.tasks = JSON.parse(localStorage.getItem('todotasks')) ? JSON.parse(localStorage.getItem('todotasks')) : [];
-  }
-}
-
-export default MyToDoList;
